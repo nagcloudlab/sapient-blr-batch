9dc952cfb49ed7e78b0eb75c8b3563915d3608da
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-07-31 15:46:06 +0530

    play-with-js

---
+++ 02-tech/04-play-with-javascript/index.js
@@ -0,0 +1,26 @@
+
+
+// # hello
+function hello() {
+    console.log("Hello");
+    console.log("😀");
+}
+hello();
+
+// # hi
+function hi() {
+    console.log("Hi");
+    console.log("😀");
+}
+hi();
+
+// # hey
+function hey() {
+    console.log("Hey");
+    console.log("😀");
+}
+hey();
+
+// design issues
+// code tangling aka coupling
+// code scattering aka cohesion
